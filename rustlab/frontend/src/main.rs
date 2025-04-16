85897debfca20bc9371b1c22efd1893849c4502c
Arnav-panjla <arnavpanjla@gmail.com>
2025-04-16 09:53:45 +0530

    fixed web interface

--- rustlab/frontend/src/main.rs
+++ rustlab/frontend/src/main.rs
@@ -5,6 +5,7 @@ fn app() -> Html {
     html! {
         <div>
             <h1>{ "Rust Spreadsheet Web UI" }</h1>
+            <h1 style="color: black; font-size: 35px;">{ "hello world computer" }</h1>
             <p>{ "Welcome to the browser version!" }</p>
         </div>
     }
